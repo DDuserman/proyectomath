5e99fc5010b90d8589bbc7f19a4c71fe1f42edc1
examen <examen@CampusUninorte.local>
2023-09-13 12:23:56 -0500

    odio dart

--- lib/ui/pages/page3.dart
+++ lib/ui/pages/page3.dart
@@ -2,12 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class Page3 extends StatelessWidget {
-  Page3({super.key, required this.op1,required this.op2,required this.operation});
+  Page3({super.key, required this.op1,required this.op2,required this.operation, required this.result});
   //Aquí se supone que van las variables
+
+  final int op1;
+  final int op2;
+  final String operation;
+  final int result;
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    int op1 = 0;
+    int op2 = 0;
+    String operation= "+";
+    int result = 0;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Test matemático'),
@@ -17,7 +26,10 @@ class Page3 extends StatelessWidget {
           Column(
             children: [
               SizedBox(
-                //Text()
+                child: Text(
+                  "$op1 $operation $op2"
+                ),
+                
               )
             ]
           )
